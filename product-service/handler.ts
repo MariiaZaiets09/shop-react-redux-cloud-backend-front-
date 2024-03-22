69ac2a3c12d6930c9e83501d145e71c5f8f39c1e
Mariia Zaiets <mariia_zaiets@epam.com>
2024-03-22 19:21:03 +0100

    Add API link to README

--- product-service/handler.ts
+++ product-service/handler.ts
@@ -1,6 +1,4 @@
 import * as handlers from './src';
 
-console.log(process.env);
-
 export const getProductById = handlers.getProductByIdHandler();
 export const getProductsList = handlers.getProductsListHandler();
